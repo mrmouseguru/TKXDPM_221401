24b8131f038983ca5ed01d259023c1b64feeb638
CGM <mrmouse.guru@gmail.com>
2024-09-14 10:21:37 +0700

    update basic soure code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Addition Application");
+        AddUIConsoleInput addUIConsoleInput = new AddUIConsoleInput();
+        addUIConsoleInput.input();
     }
 }
